dd104ba33bcac27a0806be8a82bdb25104f35b2d
WilliamK1066 <60358350+WilliamK1066@users.noreply.github.com>
2020-03-02 21:56:20 -0800

    Started Board
    Made the board class with Michael, set up some stuff to be added when other classes are made.

---
+++ CI-Fluxx-Card-Game/Assets/Board/Board/Assets/BoardScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+
+
+public class BoardScript : MonoBehaviour
+{
+    public bool Goal() => true;
+
+    public static void CreateDeck()
+    {
+        //Deck theDeck = new Deck(20);
+        //theDeck.reshuffle();
+
+
+    }
+    public void CreatePlayer()
+    {
+        for (int k = 0; k < numberOfPlayers; k++)
+        {
+            //Player player = new Player(ID: k+1);
+            // player.hand(0);
+        }
+
+    }
+
+    int numberOfPlayers = 2;
+    
+    bool isGameGoing = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+        // if(Deck.AvailableCards <= CardsDrawn) => Deck.Reshuffle();
+
+        //playerArea.AddtoHand(playerID);
+
+        while (isGameGoing)
+        {
+            //play(playerID);
+            //playerID = (playerID + 1) % NumberOfPlayers;
+
+
+            isGameGoing = false;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
