be8329e55c4cac16aa503d85661249f113c44f8e
Jared Gassman <jaredgassman@gmail.com>
2020-03-04 09:13:08 -0800

    Started drag and drop functions

---
+++ CI-Fluxx-Card-Game/Assets/Scripts/DropZoneKeepers.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class DropZoneKeepers : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
+{
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if(eventData.pointerDrag == null)
+        {
+            return;
+        }
+
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+
+    }
+}
