6dc4a8310acdc2a3c82257b83e2ebe1d5b18c75d
Jared Gassman <jaredgassman@gmail.com>
2020-03-04 21:43:29 -0800

    Starting drag event handler

--- CI-Fluxx-Card-Game/Assets/Scripts/Draggable.cs
+++ CI-Fluxx-Card-Game/Assets/Scripts/Draggable.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class Draggable : MonoBehaviour
+
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform parentToReturnTo = null;
+    public Transform placeHolderParent = null;
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnEndDrag(PointerEventData eventData)
     {
-        
+
     }
 }
