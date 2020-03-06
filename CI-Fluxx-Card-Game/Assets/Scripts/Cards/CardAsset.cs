07cd5b6859f31059299a365ceb27d44b00194ec9
Ferid Ruano <feridruano@pop-os.localdomain>
2020-03-06 00:46:22 -0800

    Card Assets, Card UI Object, Asset Script, Asset Manager Script, and Rotation Script

---
+++ CI-Fluxx-Card-Game/Assets/Scripts/Cards/CardAsset.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CardTypes
+{
+    action,
+    creeper,
+    goal,
+    keeper,
+    rule
+}
+
+[CreateAssetMenu(fileName = "New Card Asset", menuName = "Card Asset")]
+public class CardAsset : ScriptableObject
+{
+    [Header("Card Asset Information")] public new string name;
+    [TextArea(2, 3)] public string description;
+    public CardTypes type;
+    public Sprite icon;
+    public Sprite avatar;
+
+    [Header("Goal Information")] public string[] keepers;
+}
---
+++ CI-Fluxx-Card-Game/Assets/Scripts/Cards/CardRotation.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class CardRotation : MonoBehaviour
+{
+    // C# Objects that will hold Unity UI Game Objects
+    public RectTransform cardFront;
+    public RectTransform cardBack;
+    public Transform targetFacePoint;
+    public Collider col;
+
+    // Current Card View
+    private bool showingBack = false;
+
+    private void Update()
+    {
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(origin: Camera.main.transform.position,
+            direction: (-Camera.main.transform.position + targetFacePoint.position).normalized,
+            maxDistance: (-Camera.main.transform.position + targetFacePoint.position).magnitude);
+        
+        // Check for Raycast Hit on Collider
+        bool passedThroughColliderOnCard = false;
+        foreach (RaycastHit h in hits)
+        {
+            if (h.collider == col)
+                passedThroughColliderOnCard = true;
+        }
+
+        // Swap (Rotate) Card Unity UI Game Objects
+        if (passedThroughColliderOnCard != showingBack)
+        {
+            showingBack = passedThroughColliderOnCard;
+            if (true == showingBack)
+            {
+                // Show Card Back
+                cardFront.gameObject.SetActive(false);
+                cardBack.gameObject.SetActive(true);
+            }
+            else
+            {
+                // Show Card Front
+                cardFront.gameObject.SetActive(true);
+                cardBack.gameObject.SetActive(false);
+            }
+        }
+    }
+}
