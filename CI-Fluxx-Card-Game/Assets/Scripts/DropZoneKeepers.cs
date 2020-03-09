1f6286ac4d7ce8597b38512932b473d4a2101d97
Jared Gassman <jaredgassman@gmail.com>
2020-03-08 20:48:30 -0700

    finished drop zone
    - once cards are played they cant be taken back
    - only keepers can be played in keeper area

--- CI-Fluxx-Card-Game/Assets/Scripts/DropZoneKeepers.cs
+++ CI-Fluxx-Card-Game/Assets/Scripts/DropZoneKeepers.cs
@@ -6,6 +6,7 @@ using UnityEngine.EventSystems;
 
 public class DropZoneKeepers : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler
 {
+    public Draggable.areas typeOfArea = Draggable.areas.DECK;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
@@ -14,15 +15,39 @@ public class DropZoneKeepers : MonoBehaviour, IDropHandler, IPointerEnterHandler
             return;
         }
 
+        Draggable draggable = eventData.pointerDrag.GetComponent<Draggable>();
+        if(draggable != null)
+        {
+            draggable.placeHolderParent = this.transform;
+        }
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        if(eventData.pointerDrag == null)
+        {
+            return;
+        }
 
+        Draggable draggable = eventData.pointerDrag.GetComponent<Draggable>();
+        if (draggable != null && draggable.placeHolderParent == this.transform)
+        {
+            draggable.placeHolderParent = draggable.parentToReturnTo;
+        }
     }
 
     public void OnDrop(PointerEventData eventData)
     {
+        Draggable draggable = eventData.pointerDrag.GetComponent<Draggable>();
+        if(draggable != null)
+        {
+            // draggable.parentToReturnTo = this.transform;
 
+            if(typeOfArea == draggable.typeOfArea)
+            {
+                draggable.parentToReturnTo = this.transform;
+            }
+        
+        }
     }
 }
