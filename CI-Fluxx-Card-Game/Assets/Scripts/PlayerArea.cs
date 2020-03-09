96b26baaf9bd74e71d726718b3c67c7947cd2bfd
Jared Gassman <jaredgassman@gmail.com>
2020-03-08 20:46:02 -0700

    player area script
    - needs things passed

--- CI-Fluxx-Card-Game/Assets/Scripts/PlayerArea.cs
+++ CI-Fluxx-Card-Game/Assets/Scripts/PlayerArea.cs
@@ -6,6 +6,39 @@ public class PlayerArea : ScriptableObject
 {
     public Keeper[] keepersPlayed;
     public Card[] playerHand;
+    public int playerId;
+    public int HAND_LIMIT = 10;
+
+    
+
+    public void createHand(int HAND_LIMIT)
+    {
+        playerHand = new Card[HAND_LIMIT];
+        drawCards();
+    }
+
+    public void playCard()
+    {
+
+    }
+
+    public void drawCards() // pass in the deck here
+    {
+        int drawRules = 2;  // get current draw rules here
+
+ 
+        for(int i = 0; i <= drawRules; i++)
+        {
+            playerHand[i] = new Card();     // draw from deck passed in
+        }
+    }
+
+    public Card[] getCardsInPlay()
+    {
+        return playerHand;
+    }
+
+
 
 
 }
