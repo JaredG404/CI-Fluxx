1c7132c7e2905545ffbc5a6dcdd10e7124088a60
unknown <cristiano_aguilar19@hotmail.com>
2020-03-13 14:03:10 -0700

    added a zoom on card hover to better see the card, still needs some fixing tho.

---
+++ CI-Fluxx-Card-Game/Assets/Scripts/CardZoom.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardZoom : MonoBehaviour
+{
+
+    public GameObject Canvas;
+    private GameObject zoomCard;
+    public void Awake()
+    {
+        Canvas = GameObject.Find("Canvas");
+    }
+
+    public void OnHoverEnter()
+    {
+        zoomCard = Instantiate(gameObject, new Vector2(Input.mousePosition.x - 450, Input.mousePosition.y - 200), Quaternion.identity);
+        zoomCard.transform.SetParent(Canvas.transform, false);
+        zoomCard.transform.localScale = new Vector2(2,2);
+        zoomCard.transform.Rotate(0, 180, 0);
+        RectTransform rect = zoomCard.GetComponent<RectTransform>();
+        rect.sizeDelta = new Vector2(240, 344);
+    }
+
+    public void OnHoverExit()
+    {
+        Destroy(zoomCard);
+        Debug.Log("zoom destroyed");
+    }
+}
