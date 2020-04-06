a4e47a7ab3acb8e6af68d3507571e148714153e0
Ferid Ruano <feridruano@gmail.com>
2020-04-06 14:16:25 -0700

    Main Menu and Game Menu

---
+++ CI-Fluxx-Card-Game/Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Using screen name is probably better. Will change later though.
+    public void PlayGame() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void ViewDeck() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    public void Tutorial() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 3);
+    }
+    public void QuitGame() {
+        Debug.Log("Quit Game.");
+        Application.Quit();
+    }
+}
