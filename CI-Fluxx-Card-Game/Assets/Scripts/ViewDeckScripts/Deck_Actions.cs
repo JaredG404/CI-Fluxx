a79108eaec9ca9261aa4fc371c26d31f5e8c4900
WilliamK1066 <60358350+WilliamK1066@users.noreply.github.com>
2020-05-04 20:14:36 -0700

    Added 2 pages of content

---
+++ CI-Fluxx-Card-Game/Assets/Scripts/ViewDeckScripts/Deck_Actions.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck_Actions : MonoBehaviour
+{
+    public GameObject Panel;
+    public GameObject[] objs;
+
+    public void DeactivateAllButtons()
+    {
+        objs = GameObject.FindGameObjectsWithTag("ActionPanel");
+        foreach (GameObject panel in objs)
+        {
+            panel.SetActive(false);
+        }
+    }
+
+    public void Open()
+    {
+        if (Panel != null)
+        {
+            bool active = Panel.activeSelf;
+            if (!active)
+            {
+                DeactivateAllButtons();
+            }
+            Panel.SetActive(!active);
+        }
+    }
+}
